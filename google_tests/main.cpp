c583c327bcd4cff20d234df80cbc42b67b2c1ff0
Mykola Morgunenko <Olli25010@gmail.com>
2022-10-03 01:01:19 +0200

    started tests for c++; initial commit

--- google_tests/main.cpp
+++ google_tests/main.cpp
@@ -2,12 +2,9 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <gtest/gtest.h>
-
-extern "C" {
 #include "c_string.h"
-}
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
